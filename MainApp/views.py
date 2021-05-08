d103c74375749753735ec0e321a411fed4643461
Maniac-tv <msermart@gmail.com>
2021-05-07 17:27:55 -0700

    static ok

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render, HttpResponse
+import random
 
 # Create your views here.
 
 def index(request):
     return render(request,'body.html')
+
+def create_pointers(request):
+    return render(request, 'create_pointers.html')
+
+def gen_code(request):
+    out=''
+    s= "2345789zsxecvumk"
+    for i in range(0,11):
+        iout = random.randrange(0,len(s))
+        out = out + s[iout]
+    out = "mnc-"+out
+    return HttpResponse(out)
+
+
+
--- mnc_game/urls.py
+++ mnc_game/urls.py
@@ -22,5 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
+    path('create_pointers', views.create_pointers),
+    path('gen_id', views.gen_code),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
