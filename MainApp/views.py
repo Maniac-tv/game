f9f44022258c4581a00a651da6563fd7d0bd28bf
Maniac-tv <msermart@gmail.com>
2021-05-12 14:48:42 -0700

    multiple files

--- MainApp/views.py
+++ MainApp/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, HttpResponse
 import random
 from MainApp.models import Form
 import os
-
+from mnc_game import settings
+from django.core.files.storage import FileSystemStorage
 
 # Create your views here.
 
@@ -25,10 +26,15 @@ def game_pointers(request):
     print(request.POST['pointer_id'])
     item = Form(pointer_id=request.POST['pointer_id'], lat=request.POST['lat'], lang=request.POST['lang'], name_location=request.POST['name_location'], description=request.POST['description'], help=request.POST['help'], area=request.POST['area'])
     item.save()
+    #Если есть файлы создаем папку с названием идентификатора и сохраняем туда
     if 'my_file' in request.FILES:
-        os.mkdir(MEDIA_ROOT)
+        os.mkdir(os.path.join(settings.MEDIA_ROOT, request.POST['pointer_id']))
+        f = request.FILES.getlist('my_file')
+        for elm in f:
+            fs = FileSystemStorage()
+            filename = fs.save(os.path.join(os.path.join(settings.MEDIA_ROOT, request.POST['pointer_id']),elm.name), elm)
 
-    return HttpResponse('')
+    return HttpResponse(f'<script>alert(\'Создан поинтер {request.POST["pointer_id"]} с названием {request.POST["name_location"]}\')</script>')
 
 
 
